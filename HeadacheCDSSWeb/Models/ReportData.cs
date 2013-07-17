e76ade0cd893f7d5dd35ae04fedc48e302bc4fa1
JaneBlue <fguanjun1004@gmail.com>
2013-07-17 15:18:03 +0800

    用药数据修改

--- HeadacheCDSSWeb/Models/ReportData.cs
+++ HeadacheCDSSWeb/Models/ReportData.cs
@@ -68,7 +68,6 @@ namespace HeadacheCDSSWeb.Models
         public string DrugApplication { get; set; }
         public string DrugCategory { get; set; }
         public string DrugName { get; set; }
-        public string Therapy { get; set; }
         public string DrugDose { get; set; }
     }
    public class PDrug{
